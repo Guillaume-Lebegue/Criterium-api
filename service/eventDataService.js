9ec6bd843202db8b47d575e18e4f208ee4d5c572
LEBEGUE Guillaume <guillaume.lebegue@epitech.eu>
2020-06-26 14:01:11 +0200

    [ADD] Create event in data base

--- service/eventDataService.js
+++ service/eventDataService.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Event = require('../models/eventModel');
+const { resolve } = require('bluebird');
 
 class eventDataService {
 
@@ -17,6 +18,24 @@ class eventDataService {
             Event.paginate(query, opts)
                 .then(result => resolve(result))
                 .catch(error => reject(error));
+        })
+    }
+
+    createEvent(name, dateStart, dateEnd, address, isPublic, creator) {
+        return new Promise((resolve, reject) => {
+            const event = new Event();
+
+            event.name = name;
+            event.dateStart = dateStart;
+            event.dateEnd = dateEnd;
+            event.address = address;
+            event.isPublic = isPublic;
+            event.creator = creator;
+
+            event.save((error, doc) => {
+                if (error) reject(error);
+                else resolve(doc);
+            })
         })
     }
 
